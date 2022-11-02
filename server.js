0ef4ea7292ed1bbff8a8502a0876b518cd0bbe7e
welsmore54 <welsmore1618@gmail.com>
2022-11-02 15:14:05 -0600

    Personal Project 1.3

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Weston Elsmore')
-})
+app.use('/', require('./routes'))
 
 app.listen(port, () => {
-    console.log('App is listening on port 3000')
+    console.log(`App is listening on port ${port}`)
 })
