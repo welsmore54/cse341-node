e9e81b40014ab061fc91d252fe357adc0d79d84a
welsmore54 <welsmore1618@gmail.com>
2022-11-03 15:44:30 -0600

    Personal Project 2.4

--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,8 @@ const routes = require('express').Router();
 
 routes.use('/contacts', require('./contacts'))
 
+routes.get('/', (req,res) => {
+    res.send('Dafne Elsmore')
+})
+
 module.exports = routes;
